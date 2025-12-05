7ceae12eac5d654f78bce5fc3fcbcd0c5e22de74
sreyaku2003-ctrl <sreyaku2003@gmail.com>
2025-12-05 15:38:20 +0530

    Update dw.py

--- dw.py
+++ dw.py
@@ -1,4 +1,6 @@
 import nltk
 nltk.download('punkt', download_dir='nltk_data')
 nltk.download('wordnet', download_dir='nltk_data')
+
 nltk.download('punkt_tab', download_dir='nltk_data')
+nltk.download('stopwords', download_dir='nltk_data')
